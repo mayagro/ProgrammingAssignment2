ad1cc2ad99423a83f489f825178aa7c5957bc45b
magrosu <maya.grosul@gfk.com>
2015-06-19 12:59:17 -0700

    MESSAGE

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,13 @@
-#The makeCacheMatrix function creates a matrix object that can cache 
-#the input matrix and it's inverse.
-#Doing so helps R allocate computing memory more efficiently 
-#for future computations operated on the same matrix.
+#This function creates an object that can cache 
+#This will help use memory more efficiently 
 makeCacheMatrix <- function(x = matrix()) {
-        m <- NULL  # sets the value of m to NULL
-        set <- function(y) {   #set the value of the matrix
-                x <<- y    ## caches the inputted matrix
-                m <<- NULL ## sets the value of m (the matrix inverse if used cacheSolve) to NULL  
+        m <- NULL  # sets value of m to NULL
+        set <- function(y) {   #sets value of the matrix
+                x <<- y    ## caches  matrix which was inputed
+                m <<- NULL ## sets to null m value   
         }
-        get <- function() x   #gets the value of the matrix
-        setmatrix <- function(solve) m <<- solve  #solve(x) returns its inverse
+        get <- function() x   #gets value for the matrix
+        setmatrix <- function(solve) m <<- solve  #returns its inverse
         getmatrix <- function() m 
         list(set = set, get = get,
              setmatrix = setmatrix,
@@ -19,17 +17,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ##If the function works correctly, you should be able to pass the special matrix 
-#(made by the function makeCacheMatrix) to cacheSolve the first time without any messages
-#however, after the first time it should give you the message "getting cached data", until 
-#you clear your setmatrix function assignment.
-cacheSolve <- function(x = matrix(), ...) {  #comparing matrix with previous one
-        m <- x$getmatrix()  #this gets the inverse if it was already calculated
-        if(!is.null(m)) {   #checks if cacheSolve has been run before
-                message("getting cached data") #if it has then provides this message
-                return(m)
+# to cacheSolve the first time without any messages
+cacheSolve <- function(x = matrix(), ...) {  #comparing matrix with one before it
+        m <- x$getmatrix()  #this gets the inverse if the inverse was already calculated
+        if(!is.null(m)) {   
+                message("getting cached data") #if m was run before it will provide message
+                to return(m)
         }
         data <- x$get()
-        m <- solve(data, ...) #computes the inverse of the input matrix
+        m <- solve(data, ...) #computes the inverse 
         x$setmatrix(m) #runs the setinverse function on the inverse to cache the inverse
         m #return the inverse
 }
